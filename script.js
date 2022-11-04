5bff143969e631ed2e406ed6e432879a21922a1e
AdrianoEscarabote <drian.escarabote@hotmail.com>
2022-11-04 19:41:56 -0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// input radio
 var buttons = document.getElementsByClassName("button");
 var arr = [...buttons];
 
@@ -15,6 +16,7 @@ arr.forEach((element) => {
     });
 });
 
+// change theme
 arr.forEach((element) => {
     element.addEventListener("click", () => {
         if (element.dataset.theme === "theme-1") {
@@ -33,4 +35,53 @@ arr.forEach((element) => {
     })
 })
 
+// calc 
+const buttonKey = document.querySelectorAll(".btnKey")
+const numberDisplay = document.getElementById("numberRes")
+const upNumber = document.getElementById("up")
+const del = document.getElementById("del")
+const reset = document.getElementById("reset")
+const equal = document.getElementById("equal")
+const addition = document.getElementById("addition")
 
+const allowedKeys = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", " "]
+
+// deleting numbers
+del.addEventListener("click", () => {
+    numberDisplay.innerText = numberDisplay.textContent.slice(0, -1)
+})
+
+document.body.addEventListener("keydown", (ev) => {
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)) {
+        numberDisplay.innerText += ev.key
+    }
+    switch (ev.key) {
+        case "Backspace":
+            del.focus()
+            numberDisplay.innerText = numberDisplay.textContent.slice(0, -1)
+            break
+        case "Enter":
+            equal.focus()
+            break
+        default:
+            break
+    }
+})
+
+let numberForCalc = ""
+
+buttonKey.forEach((element) => {
+    element.addEventListener("click", () => {
+        numberDisplay.innerText += element.value
+        numberForCalc += element.value
+        console.log(numberForCalc)
+    })
+})
+
+// reset button
+reset.addEventListener("click", () => {
+    numberDisplay.innerText = ""
+    upNumber.innerText = ""
+    numberForCalc = ""
+})
