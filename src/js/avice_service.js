e3bbc02fdcce398c410a9cfeaf64a13e0907e6a3
AdrianoEscarabote <drian.escarabote@hotmail.com>
2022-12-02 10:51:26 -0300

    src

--- src/js/avice_service.js
+++ src/js/avice_service.js
@@ -1,5 +1,5 @@
 export async function getAdvice() {
-  const response = await fetch("https://api.adviceslip.com/advice")
+  const response = await fetch("https://api.adviceslip.com/advice",  { cache: "no-cache" });
   const result = await response.json()
   return result["slip"]
 }
