1fc4ec13d2c0815e75f650102fc316018134e72d
AdrianoEscarabote <drian.escarabote@hotmail.com>
2022-12-15 13:50:21 -0300

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require("path")
 
 module.exports = {
   entry: {
-    index: "./src/index.js"
+    index: "./src/index.js",
+    index: "./src/style.js"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "style.js"
   },
   mode: "production",
   module: {
