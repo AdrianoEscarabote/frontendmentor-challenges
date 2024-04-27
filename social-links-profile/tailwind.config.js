74597ce1258a42653426fecd94ebd45ac5723cd4
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-04-26 21:38:18 -0300

    chore: adding tailwind and postcss config

---
+++ social-links-profile/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			neutral: {
+				'000': 'var(--neutral-000)',
+				600: 'var(--neutral-600)',
+				900: 'var(--neutral-900)',
+				999: 'var(--neutral-999)'
+			},
+			primary: {
+				700: 'var(--primary-700)'
+			}
+		},
+		fontFamily: {
+			sans: ['Inter', 'sans-serif']
+		},
+		extend: {}
+	},
+	plugins: []
+};
