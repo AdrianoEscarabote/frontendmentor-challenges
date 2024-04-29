235734a5d3caadb84a14fe4c5281dc265a701177
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-04-29 16:43:00 -0300

    feat: adding button component

---
+++ social-links-profile/src/components/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let text = 'Click me';
+  export let href = ""
+  export let ariaLabel = ""
+</script>
+
+<a href={href}
+  aria-label={ariaLabel}
+  target="_blank"
+	class="grid place-content-center bg-neutral-600 bodyBold text-neutral-000 h-[45px] w-full rounded-lg hover:bg-primary-700 hover:text-neutral-900"
+	>{text}</a
+>
