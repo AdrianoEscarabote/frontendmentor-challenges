cb634cf470e48c58cf1ab2980f0ff4eb9d124b4d
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-04-30 12:10:28 -0300

    chore: formatting with prettier

--- social-links-profile/src/components/Button.svelte
+++ social-links-profile/src/components/Button.svelte
@@ -1,12 +1,13 @@
 <script>
 	export let text = 'Click me';
-  export let href = ""
-  export let ariaLabel = ""
+	export let href = '';
+	export let ariaLabel = '';
 </script>
 
-<a href={href}
-  aria-label={ariaLabel}
-  target="_blank"
+<a
+	{href}
+	aria-label={ariaLabel}
+	target="_blank"
 	class="grid place-content-center bg-neutral-600 bodyBold text-neutral-000 h-[45px] w-full rounded-lg hover:bg-primary-700 hover:text-neutral-900"
 	>{text}</a
 >
