fbd30f8427b2c548dd953f8a30e10bb9827ed920
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-05-29 16:49:23 -0300

    feat(components): adding header component

---
+++ loopstudios-landing-page/src/components/Header.svelte
@@ -0,0 +1,28 @@
+<header class="bg-black text-white w-full max-w-[1440px] px-[165px] py-16">
+	<div class="flex flex-col gap-[130px] mb-[78px]">
+		<nav class="flex items-center justify-between">
+			<img src="/assets/images/logo.svg" alt="" />
+			<ul class="flex items-center gap-[28.5px] mt-2">
+				<li>
+					<a href="/" aria-label="about page"> About </a>
+				</li>
+				<li>
+					<a href="/" aria-label="careers page"> Careers</a>
+				</li>
+				<li>
+					<a href="/" aria-label="events page"> Events </a>
+				</li>
+				<li>
+					<a href="/" aria-label="products page"> Products</a>
+				</li>
+				<li>
+					<a href="/" aria-label="support page">Support</a>
+				</li>
+			</ul>
+		</nav>
+
+		<article class="p-8 pl-[41px] pb-7 pr-0 border border-solid border-white max-w-[650px]">
+			<h1 class="headingM pt-[6px] tracking-[0.3px]">Immersive experiences that deliver</h1>
+		</article>
+	</div>
+</header>
