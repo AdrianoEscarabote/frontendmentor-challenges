e9f058b7670ac425472e540aac33294761b05985
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-05-29 16:41:49 -0300

    feat(components): adding footer component

---
+++ loopstudios-landing-page/src/components/Footer.svelte
@@ -0,0 +1,52 @@
+<footer class="bg-black text-white w-full flex justify-center">
+	<div
+		class="md:px-[165px] px-[40px] py-[41px] max-w-[1440px] flex flex-col md:flex-row w-full items-center justify-between"
+	>
+		<nav class="flex flex-col gap-7">
+			<img class="w-[144px]" src="/assets/images/logo.svg" alt="" />
+			<ul class="flex flex-col md:flex-row items-center gap-[29px] pt-3 md:pt-0">
+				<li>
+					<a href="/" aria-label="about page">About</a>
+				</li>
+				<li>
+					<a href="/" aria-label="careers page">Careers</a>
+				</li>
+				<li>
+					<a href="/" aria-label="events page">Events</a>
+				</li>
+				<li>
+					<a href="/" aria-label="products page">Products</a>
+				</li>
+				<li>
+					<a href="/" aria-label="support page">Support</a>
+				</li>
+			</ul>
+		</nav>
+
+		<article class="flex flex-col gap-10 items-center md:items-end py-8 md:py-0 md:gap-7">
+			<ul class="flex gap-4 items-center justify-end">
+				<li>
+					<a href="/" aria-label="go to facebook">
+						<img src="/assets/images/icon-facebook.svg" alt="" />
+					</a>
+				</li>
+				<li>
+					<a href="/" aria-label="go to twitter">
+						<img src="/assets/images/icon-twitter.svg" alt="" />
+					</a>
+				</li>
+				<li>
+					<a href="/" aria-label="go to pinterest">
+						<img src="/assets/images/icon-pinterest.svg" alt="" />
+					</a>
+				</li>
+				<li>
+					<a href="/" aria-label="go to instagram">
+						<img src="/assets/images/icon-instagram.svg" alt="" />
+					</a>
+				</li>
+			</ul>
+			<p class="text-center">© 2021 Loopstudios. All rights reserved.</p>
+		</article>
+	</div>
+</footer>
