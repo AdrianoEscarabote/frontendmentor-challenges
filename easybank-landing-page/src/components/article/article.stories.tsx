fb7c30f3186cf3bd036364861870e25bff0f4a17
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-06-24 18:31:00 -0300

    feat(components): adding article stories

---
+++ easybank-landing-page/src/components/article/article.stories.tsx
@@ -0,0 +1,17 @@
+import Article from ".";
+import { Meta, StoryObj } from "@storybook/react";
+import { ArticleProps } from "./article";
+
+export default {
+  title: "components/Article",
+  component: Article,
+  tags: ["autdocs"],
+  args: {
+    author: "By Adriano",
+    imagePath: "/images/image-restaurant.jpg",
+    title: "Receive money in any currency with no fees",
+    text: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+  },
+} as Meta<ArticleProps>;
+
+export const Primary: StoryObj = {};
