def1ebd70e5164ebc205ac2dae7bc6f48b8b4ccc
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-06-24 18:36:27 -0300

    feat(components): adding latest articles and why choose easy bank stories

---
+++ easybank-landing-page/src/components/latestArticles/latestArticles.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import LatestArticles from ".";
+
+export default {
+  title: "components/latestArticles",
+  component: LatestArticles,
+} as Meta;
+
+export const Primary: StoryObj = {};
---
+++ easybank-landing-page/src/components/whyChooseEasyBank/whyChooseEasyBank.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import WhyChooseEasyBank from ".";
+
+export default {
+  title: "components/whyChooseEasyBank",
+  component: WhyChooseEasyBank,
+} as Meta;
+
+export const Primary: StoryObj = {};
