6e0227fe72651ae774e045e15979ea26234639b3
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-06-24 18:30:31 -0300

    feat(components): adding header stories

---
+++ easybank-landing-page/src/components/header/header.stories.tsx
@@ -0,0 +1,10 @@
+import Header from ".";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "components/Header",
+  component: Header,
+  tags: ["autdocs"],
+} as Meta;
+
+export const Primary: StoryObj = {};
