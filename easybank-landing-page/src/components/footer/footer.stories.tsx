4f2ca9da246268f17a8894da928a959e595c55f4
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-06-24 18:30:10 -0300

    feat(components): adding footer stories

---
+++ easybank-landing-page/src/components/footer/footer.stories.tsx
@@ -0,0 +1,10 @@
+import Footer from ".";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "components/footer",
+  component: Footer,
+  tags: ["autdocs"],
+} as Meta;
+
+export const Primary: StoryObj = {};
