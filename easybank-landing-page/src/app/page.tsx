20e885c3c658f942d30d456c6ba47c8f815a5581
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-06-24 18:03:09 -0300

    feat(app): adding page

---
+++ easybank-landing-page/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/header";
+import Footer from "@/components/footer/";
+import WhyChooseEasyBank from "@/components/whyChooseEasyBank";
+import LatestArticles from "@/components/latestArticles";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <WhyChooseEasyBank />
+        <LatestArticles />
+      </main>
+      <Footer />
+    </>
+  );
+}
