07695c0271713e90d9dc56f13d3cf8a3a0306dea
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-07-23 18:25:09 -0300

    feat(components): adding request button component

---
+++ easybank-landing-page/src/components/requestButton/index.tsx
@@ -0,0 +1,9 @@
+const RequestButton = () => {
+  return (
+    <button className="bg-gradient-button w-[164px] h-[46px] rounded-3xl text-white">
+      Request invite
+    </button>
+  );
+};
+
+export default RequestButton;
---
+++ easybank-landing-page/src/components/requestButton/requestButton.stories.tsx
@@ -0,0 +1,10 @@
+import RequestButton from ".";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "components/RequestButton",
+  component: RequestButton,
+  tags: ["autdocs"],
+} as Meta;
+
+export const Primary: StoryObj = {};
