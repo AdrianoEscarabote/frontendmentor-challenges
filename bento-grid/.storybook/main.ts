36544101cc317f0a06d3b24386f9a3bb2eebdf28
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-09-23 17:42:06 -0300

    chore: update path

--- bento-grid/.storybook/main.ts
+++ bento-grid/.storybook/main.ts
@@ -1,10 +1,7 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
@@ -17,5 +14,5 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["..\\public"],
-};
-export default config;
+}
+export default config
