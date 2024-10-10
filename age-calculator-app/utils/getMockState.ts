43e2d4fb3eec732ca6b425bf662afd1179885805
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-10-10 12:54:41 -0300

    chore(utils): add getmockstate fn

---
+++ age-calculator-app/utils/getMockState.ts
@@ -0,0 +1,14 @@
+export default function getMockState() {
+  return {
+    calculatorSlice: {
+      day: 0,
+      month: 0,
+      year: 0,
+      age: {
+        days: 0,
+        months: 0,
+        years: 0,
+      },
+    },
+  }
+}
