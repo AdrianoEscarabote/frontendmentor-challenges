28e6c3ab1621ab068287c7f1ae18478e4b62ba63
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-10-14 12:45:29 -0300

    chore: add app.vue

---
+++ insure-landing-page/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from './components/AppHeader.vue'
+import Footer from './components/AppFooter.vue'
+import Main from './components/AppMain.vue'
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
