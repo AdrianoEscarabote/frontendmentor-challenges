8740601c1ccad404f8a0cbd82f5c9067b7161cee
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-11-11 21:59:59 -0300

    chore: add get mock state fn

---
+++ mortgage-repayment-calculator/utils/getMockState.ts
@@ -0,0 +1,12 @@
+export default function getMockState() {
+  return {
+    calculatorSlice: {
+      amount: 300000,
+      term: 25,
+      rate: 5.25,
+      paymentType: "repayment",
+      monthlyPayment: 1797.743146,
+      totalRepayable: 539322.943654,
+    },
+  }
+}
