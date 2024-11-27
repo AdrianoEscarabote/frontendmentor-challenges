5fa9633ec2b1587b878fdbac92237676588a14df
AdrianoEscarabote <drian.escarabote@hotmail.com>
2024-11-27 12:06:14 -0300

    chore: add storybook config

---
+++ ecommerce-product-page/.storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
+// global css
+import "../app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+  },
+};
+
+export default preview;
