f67ed451136c190c2731637b389db4112c597718
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-04-09 21:26:32 -0300

    chore: add jest config & setup

---
+++ browser-extensions-manager-ui/jest.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest")
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+}
+
+module.exports = createJestConfig(customJestConfig)
---
+++ browser-extensions-manager-ui/jest.setup.js
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom"
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}))
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    addListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
