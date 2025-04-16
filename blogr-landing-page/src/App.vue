90cfd59d073937b43ddfd2c8dd5f6bfcc9fb10f1
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-04-16 12:53:01 -0300

    chore: add app.vue & main.ts

---
+++ blogr-landing-page/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HeaderComponent from './components/Header/HeaderComponent.vue'
+import FooterComponent from './components/Footer/FooterComponent.vue'
+import InfrastructureSection from './components/InfrastructureSection/InfrastructureSection.vue'
+import FeatureWithIllustration from './components/FeatureWithIllustration/FeatureWithIllustration.vue'
+import FeatureHighlight from './components/FeatureHighlight/FeatureHighlight.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+
+  <main>
+    <FeatureHighlight />
+    <InfrastructureSection />
+    <FeatureWithIllustration />
+  </main>
+
+  <FooterComponent />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
