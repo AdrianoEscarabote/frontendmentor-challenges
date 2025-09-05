fd53eabffd8ac0aad9471daca73ce6a45ee76caa
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-09-05 18:04:54 -0300

    chore: setup jest with testing-library

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/app/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
